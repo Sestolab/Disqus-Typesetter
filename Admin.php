d483eb9b34ff87e4d605b2ace9a327e5ee56f480
Sestowner <owner@sestolab.pp.ua>
2019-01-23 16:28:00 +0200

    Fixes errors: in_array() expects parameter 2 to be array, null given Undefined index: pages_comments

--- Admin.php
+++ Admin.php
@@ -18,15 +18,15 @@ class Admin{
 			break;
 		}
 		$this->get_lang_ext();
-		
+
 		echo '<p style="text-align: center;"><img  src="'.$addonRelativeCode.'/img/disqus-logo-blue-white.svg'.'" /></p><br/><br/>';
-		echo '<div class="disqus_frame" align="center";>'; 
+		echo '<div class="disqus_frame" align="center";>';
 		echo '<input type="radio" name="Disqus" checked="checked" id="Disqus_Site_Conf"/><label for="Disqus_Site_Conf">'.$this->lang_dict['Site Configuration'].'</label>';
 		echo '<input type="radio" name="Disqus" id="Disqus_show_comments"/><label for="Disqus_show_comments">'.$this->lang_dict['Select Pages to show comments'].'</label>';
 		echo '<input type="radio" name="Disqus" id="Disqus_Manage"/><label for="Disqus_Manage">'.$this->lang_dict['Manage Community'].'</label>';
-		
+
 		echo '<div><br/><br/>';
-		
+
 		echo '<form action="'.\common::GetUrl('Admin_Disqus').'" method="post">';
 			if ($this->disqus_forum_url ==''){
 					echo '<label for="disqus_forum_url">'.$this->lang_dict['Shortname'].'<span style="color: red;"> *  </span></label><input id="disqus_forum_url" name="disqus_forum_url" class="gpinput" required value="'.$this->disqus_forum_url.'" type="text"><p class="description">'.$this->lang_dict['Your site\'s unique identifier '].'<a href="https://help.disqus.com/customer/portal/articles/466208" target="_blank">'.$this->lang_dict['What is this?'].'</a></p>
@@ -38,31 +38,31 @@ class Admin{
 						<input id="disqus_forum_url" name="disqus_forum_url" class="gpinput" required value="'.$this->disqus_forum_url.'" type="text">
 						<p class="description">'.$this->lang_dict['Your site\'s unique identifier '].'<a href="https://help.disqus.com/customer/portal/articles/466208" target="_blank">
 						'.$this->lang_dict['What is this?'].'</a></p>';
-			
+
 				echo '</div><div><br/><br/>';
 
-		
-	
+
+
  echo '<table class="comments_pages">
 		<col style="width:50%"/><col style="width:25%"/><col style="width:25%"/>
-			<tr style="font-weight:bold"><td>'.$this->lang_dict['Pages'].'</td><td>'.$this->lang_dict['Show comments?'].'</td><td></td></tr>';
+			<tr style="font-weight:bold"><td>'.$this->lang_dict['Pages'].'</td><td>'.$this->lang_dict['Show comments?'].'</td></tr>';
 
 foreach ($gp_index as $t=>$i) {
 	$url=\common::GetUrl($t);
-	
+
 	echo '<tr>
 			<td ><a href="'.$url.'" title="'.$url.'" target="_blank">'.\common::GetLabel($t).'</a></td><td class="comment_selector">';
-	if (in_array($i, $this->pages_comments)){
-		echo '<input id="'.$i.'" type="checkbox" checked name="pages_comments[]" value="'.$i.'" />';
-	} else {
-		echo '<input id="'.$i.'" type="checkbox" name="pages_comments[]" value="no" />';
-	}
-			
-	echo '</td><td></td></tr>';
+
+		if (in_array($i, $this->pages_comments)){
+			echo '<input id="'.$i.'" type="checkbox" checked name="pages_comments[]" value="'.$i.'" />';
+		}else {
+			echo '<input id="'.$i.'" type="checkbox" name="pages_comments[]" value="no" />';
+		}
+	echo '</td></tr>';
 }
-  
+
   echo '</table>';
-			echo '</div>';				
+			echo '</div>';
 	echo '<div><br/><br/>';
 			echo '
 			<div class="manage-panel-content">
@@ -102,25 +102,27 @@ foreach ($gp_index as $t=>$i) {
 		echo '<br/><br/>
 					<input type="hidden" name="cmd" value="saveConfig" />
 					<input type="submit" value="'.$langmessage['save_changes'].'" class="gpsubmit"/>
-					<input type="button" onClick="location.href=\'' . \common::GetUrl('Admin_Disqus') . '\'" name="cmd" 
-					value="'.$langmessage['cancel'].'" class="admin_box_close gpcancel" /></form>'; 
+					<input type="button" onClick="location.href=\'' . \common::GetUrl('Admin_Disqus') . '\'" name="cmd"
+					value="'.$langmessage['cancel'].'" class="admin_box_close gpcancel" /></form>';
 	}
-	
+
 	function saveConfig(){
 		global $addonPathData, $langmessage;
 
 		$configFile = $addonPathData.'/config.php';
 		$config     = array();
 		$config['disqus_forum_url'] = $_POST['disqus_forum_url'];
-	
-		if($_POST){
-			$config['pages_comments'] = $_POST["pages_comments"];	
+
+		if(isset($_POST["pages_comments"])){
+			$config['pages_comments'] = $_POST["pages_comments"];
+		}else{
+			$config['pages_comments'] = array();
 		}
-	
+
 		$this->pages_comments = $config['pages_comments'];
 		$this->disqus_forum_url	= $config['disqus_forum_url'];
-	
-	
+
+
 		if( !\gpFiles::SaveArray($configFile,'config',$config) ){
 			message($langmessage['OOPS']);
 			return false;
@@ -132,33 +134,33 @@ foreach ($gp_index as $t=>$i) {
 
   function loadConfig(){
 		global $addonPathData;
-		
+
 		$configFile = $addonPathData.'/config.php';
-    
+
 		if(file_exists($configFile)){
 			include_once $configFile;
 		}else{
 			$this->disqus_forum_url ='';
-			$this->pages_comments=array();		
-		}		
-		
+			$this->pages_comments=array();
+		}
+
 
 		if (isset($config)) {
 			$this->disqus_forum_url = $config['disqus_forum_url'];
-			$this->pages_comments = $config['pages_comments']; 
+			$this->pages_comments = $config['pages_comments'];
 		}
-  } 
-  
+  }
+
 	function get_lang_ext(){
 		global $config;
 		$langfile = '/languages/'.$config['language'].'.php';
-	
+
 		if(file_exists(dirname(__FILE__).$langfile)){
 			include dirname(__FILE__).$langfile;
 			}else{$langfile = '/languages/en.php';
 			include dirname(__FILE__).$langfile;
 			}
 			$this->lang_dict = $lang_dict;
-	}  
-	
+	}
+
 }
--- Disqus.php
+++ Disqus.php
@@ -4,27 +4,26 @@ defined('is_running') or die('Not an entry point...');
 
 
 class Disqus{
-	
+
 	function __construct(){
 			global $page,$addonPathData;
 			$configFile = $addonPathData.'/config.php';
-		
+
 			if(file_exists($configFile)){
 				include_once $configFile;
 			}else {
 			$this->disqus_forum_url ='';
 			$this->pages_comments=array();
-			}		
-		
+			}
+
 			if (isset($config)){
 				$this->disqus_forum_url = $config['disqus_forum_url'];
 				$this->pages_comments = $config['pages_comments'];
 			}
-		 
+
 			if (in_array($page->gp_index, $this->pages_comments)){
 				$this->ShowDisqus($page->gp_index,$page->title);
-			}
-		
+				}
 		}
 
 function ShowDisqus($index,$title){
@@ -33,10 +32,10 @@ function ShowDisqus($index,$title){
 				var disqus_config = function () {
 				this.page.title = "'.$title.'";
 				this.page.url = "'.$this->getUrl().'";
-				this.page.identifier = "'.$index.'"; 
+				this.page.identifier = "'.$index.'";
 				};
 
-				(function() { 
+				(function() {
 				var d = document, s = d.createElement(\'script\');
 				s.src = \'https://'.$this->disqus_forum_url.'.disqus.com/embed.js\';
 				s.setAttribute(\'data-timestamp\', +new Date());
@@ -45,7 +44,7 @@ function ShowDisqus($index,$title){
 		</script>
 	<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>';
 		}
-	
+
 	function getUrl() {
 		$url  = @( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
 		$url .= ( $_SERVER["SERVER_PORT"] != 80 ) ? ":".$_SERVER["SERVER_PORT"] : "";
