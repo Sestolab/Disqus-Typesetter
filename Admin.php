3f559df3a5c601a3125d495c97293f1ad37aa702
Sestowner <owner@sestolab.pp.ua>
2019-07-19 20:27:45 +0300

    v1.0

--- Admin.php
+++ Admin.php
@@ -6,161 +6,124 @@ defined('is_running') or die('Not an entry point...');
 class Admin{
 
 	function __construct(){
-		global $page, $addonRelativeCode, $langmessage, $gp_index, $gp_titles;
+		global $page, $addonRelativeCode, $langmessage, $gp_index;
 		$page->head_js[] = $addonRelativeCode.'/Disqus.js';
 		$page->css_admin[] = $addonRelativeCode.'/Disqus.css';
+		$lang = $this->getLang();
+		
 		$this->loadConfig();
-		$cmd = \common::GetCommand();
+		
+		if(\common::GetCommand() == 'saveConfig')
+				$this->saveConfig();
 
-		switch($cmd){
-			case 'saveConfig':
-			$this->saveConfig();
-			break;
+		echo '<p class="text-center"><img  src="'.$addonRelativeCode.'/img/disqus-logo-blue-white.svg'.'" /></p><br/>';
+		echo '<div class="disqus_frame" align="center">';
+		if ($this->disqus_forum_url == ''){
+			echo '<input type="radio" name="Disqus" id="Disqus_Site_Conf" checked/><label for="Disqus_Site_Conf">'.$lang['Site Configuration'].'</label>';
+			echo '<input type="radio" name="Disqus" id="Disqus_show_comments" disabled /><label for="Disqus_show_comments">'.$lang['Select Pages to show comments'].'</label>';
+			echo '<input type="radio" name="Disqus" id="Disqus_Manage" disabled /><label for="Disqus_Manage">'.$lang['Manage Community'].'</label>';
+		}else{
+			echo '<input type="radio" name="Disqus" id="Disqus_Site_Conf"/><label for="Disqus_Site_Conf">'.$lang['Site Configuration'].'</label>';
+			echo '<input type="radio" name="Disqus" id="Disqus_show_comments" checked/><label for="Disqus_show_comments">'.$lang['Select Pages to show comments'].'</label>';
+			echo '<input type="radio" name="Disqus" id="Disqus_Manage"/><label for="Disqus_Manage">'.$lang['Manage Community'].'</label>';
 		}
-		$this->get_lang_ext();
 
-		echo '<p style="text-align: center;"><img  src="'.$addonRelativeCode.'/img/disqus-logo-blue-white.svg'.'" /></p><br/><br/>';
-		echo '<div class="disqus_frame" align="center";>';
-		echo '<input type="radio" name="Disqus" checked="checked" id="Disqus_Site_Conf"/><label for="Disqus_Site_Conf">'.$this->lang_dict['Site Configuration'].'</label>';
-		echo '<input type="radio" name="Disqus" id="Disqus_show_comments"/><label for="Disqus_show_comments">'.$this->lang_dict['Select Pages to show comments'].'</label>';
-		echo '<input type="radio" name="Disqus" id="Disqus_Manage"/><label for="Disqus_Manage">'.$this->lang_dict['Manage Community'].'</label>';
-
-		echo '<div><br/><br/>';
-
+		echo '<div>';
 		echo '<form action="'.\common::GetUrl('Admin_Disqus').'" method="post">';
-			if ($this->disqus_forum_url ==''){
-					echo '<label for="disqus_forum_url">'.$this->lang_dict['Shortname'].'<span style="color: red;"> *  </span></label><input id="disqus_forum_url" name="disqus_forum_url" class="gpinput" required value="'.$this->disqus_forum_url.'" type="text"><p class="description">'.$this->lang_dict['Your site\'s unique identifier '].'<a href="https://help.disqus.com/customer/portal/articles/466208" target="_blank">'.$this->lang_dict['What is this?'].'</a></p>
-					<p><a href="https://disqus.com/profile/signup/?next=https%3A%2F%2Fdisqus.com%2Fadmin%2Fcreate%2F">'.$this->lang_dict['Sign up to register your account and site with Disqus'].'</a></p>
-					<p><a href="https://disqus.com/admin/create/">'.$this->lang_dict['Create a new site on Disqus'].'</a></p>
-						';
-			}else{
-				echo '<br/><label for="disqus_forum_url">'.$this->lang_dict['Shortname'].'<span style="color: red;"> *  </span></label>
-						<input id="disqus_forum_url" name="disqus_forum_url" class="gpinput" required value="'.$this->disqus_forum_url.'" type="text">
-						<p class="description">'.$this->lang_dict['Your site\'s unique identifier '].'<a href="https://help.disqus.com/customer/portal/articles/466208" target="_blank">
-						'.$this->lang_dict['What is this?'].'</a></p>';
-
-				echo '</div><div><br/><br/>';
-
-
-
- echo '<table class="comments_pages">
-		<col style="width:50%"/><col style="width:25%"/><col style="width:25%"/>
-			<tr style="font-weight:bold"><td>'.$this->lang_dict['Pages'].'</td><td>'.$this->lang_dict['Show comments?'].'</td></tr>';
-
-foreach ($gp_index as $t=>$i) {
-	$url=\common::GetUrl($t);
-
-	echo '<tr>
-			<td ><a href="'.$url.'" title="'.$url.'" target="_blank">'.\common::GetLabel($t).'</a></td><td class="comment_selector">';
-
-		if (in_array($i, $this->pages_comments)){
-			echo '<input id="'.$i.'" type="checkbox" checked name="pages_comments[]" value="'.$i.'" />';
-		}else {
-			echo '<input id="'.$i.'" type="checkbox" name="pages_comments[]" value="no" />';
+		echo '<label for="disqus_forum_url">'.$lang['Shortname'].'</label><input id="disqus_forum_url" name="disqus_forum_url" class="gpinput" required readonly value="'.$this->disqus_forum_url.'">';
+		echo '<p>'.$lang['Your site\'s unique identifier'].' <a href="https://help.disqus.com/customer/portal/articles/466208" target="_blank">'.$lang['What is this?'].'</a></p>';
+		echo '<p><a id="edit_shortname"><span class="fa fa-lock"></span> '.$lang['Click to make changes'].'</a></p>';
+		if ($this->disqus_forum_url == ''){
+				echo '<p><a href="https://disqus.com/profile/signup/?next=https://disqus.com/admin/create/">'.$lang['Sign up to register your account and site with Disqus'].'</a></p>';
+				echo '<p><a href="https://disqus.com/admin/create/">'.$lang['Create a new site on Disqus'].'</a></p>';
 		}
-	echo '</td></tr>';
-}
-
-  echo '</table>';
-			echo '</div>';
-	echo '<div><br/><br/>';
-			echo '
-			<div class="manage-panel-content">
-				<table border="0" style="width:100%;">
+		echo '</div>';
+ 		echo '<div><table class="comments_pages bordered"><col/><col/><tr><th>'.$lang['Pages'].'</th><th>'.$lang['Show comments?'].'</th></tr>';
+		foreach ($gp_index as $t=>$i) {
+			echo '<tr><td>'.\gp\tool::Link_Page($t).'</td><td class="comment_selector">';
+				if (in_array($i, $this->pages_comments))
+					echo '<input id="'.$i.'" type="checkbox" checked name="pages_comments[]" value="'.$i.'" />';
+				else
+					echo '<input id="'.$i.'" type="checkbox" name="pages_comments[]" value="no" />';
+			echo '</td></tr>';
+		}
+  		echo '</table></div><div>';
+			
+		echo '<div>
+				<table class="full_width">
 					<tbody>
 						<tr>
 							<td>
-							<h1>'.$this->lang_dict['Comments'].'</h1>
+							<h1>'.$lang['Comments'].'</h1>
 							</td>
 							<td>
-							<h1>'.$this->lang_dict['Analytics'].'</h1>
+							<h1>'.$lang['Analytics'].'</h1>
 							</td>
 							<td>
-							<h1>'.$this->lang_dict['Settings'].'</h1>
+							<h1>'.$lang['Settings'].'</h1>
 							</td>
 						</tr>
 						<tr>
-							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/moderate/" target="_blank">'.$this->lang_dict['Moderate'].'</a></td>
-							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/analytics/comments/" target="_blank">'.$this->lang_dict['Engagement'].'</a></td>
-							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/settings/general/" target="_blank">'.$this->lang_dict['General'].'</a></td>
+							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/moderate/" target="_blank">'.$lang['Moderate'].'</a></td>
+							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/analytics/comments/" target="_blank">'.$lang['Engagement'].'</a></td>
+							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/settings/general/" target="_blank">'.$lang['General'].'</a></td>
 						</tr>
 						<tr>
-							<td>'.$this->lang_dict['Manage: '].'<a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/access/banned/" target="_blank">'.$this->lang_dict['Banned users'].'</a> | <a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/settings/access/" target="_blank">'.$this->lang_dict['Restricted words filter'].'</a> | <a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/settings/moderators/" target="_blank">'.$this->lang_dict['Site moderators'].'</a></td>
-							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/analytics/revenue/" target="_blank">'.$this->lang_dict['Revenue'].'</a></td>
-							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/settings/community/" target="_blank">'.$this->lang_dict['Community Rules'].'</a></td>
+							<td>'.$lang['Manage:'].' <a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/access/banned/" target="_blank">'.$lang['Banned users'].'</a> | <a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/settings/access/" target="_blank">'.$lang['Restricted words filter'].'</a> | <a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/settings/moderators/" target="_blank">'.$lang['Site moderators'].'</a></td>
+							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/analytics/revenue/" target="_blank">'.$lang['Revenue'].'</a></td>
+							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/settings/community/" target="_blank">'.$lang['Community Rules'].'</a></td>
 						</tr>
 						<tr>
 							<td>&nbsp;</td>
-							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/analytics/content/" target="_blank">'.$this->lang_dict['Popular Content'].'</a></td>
-							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/settings/advanced/" target="_blank">'.$this->lang_dict['Advanced'].'</a></td>
+							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/analytics/content/" target="_blank">'.$lang['Popular Content'].'</a></td>
+							<td><a href="https://'.$this->disqus_forum_url.'.disqus.com/admin/settings/advanced/" target="_blank">'.$lang['Advanced'].'</a></td>
 						</tr>
 					</tbody>
 				</table>
+				</div></div>';
+		echo '<p>
+				<input type="hidden" name="cmd" value="saveConfig" />
+				<input type="submit" value="'.$langmessage['save_changes'].'" class="gpsubmit"/>
+				<input type="button" onClick="location.href=\''.\common::GetUrl('Admin_Disqus').'\'" name="cmd" value="'.$langmessage['cancel'].'" class="gpcancel"/>
+			</p></form></div>';
 
-		</div></div>';
-				}
-		echo '<br/><br/>
-					<input type="hidden" name="cmd" value="saveConfig" />
-					<input type="submit" value="'.$langmessage['save_changes'].'" class="gpsubmit"/>
-					<input type="button" onClick="location.href=\'' . \common::GetUrl('Admin_Disqus') . '\'" name="cmd"
-					value="'.$langmessage['cancel'].'" class="admin_box_close gpcancel" /></form>';
+		echo '<div class="text-right">Made by <a href="https://sestolab.pp.ua" target="_blank">Sestolab</a></div>';
 	}
 
 	function saveConfig(){
 		global $addonPathData, $langmessage;
-
-		$configFile = $addonPathData.'/config.php';
-		$config     = array();
+		$config = array();
+	
 		$config['disqus_forum_url'] = $_POST['disqus_forum_url'];
-
-		if(isset($_POST["pages_comments"])){
-			$config['pages_comments'] = $_POST["pages_comments"];
-		}else{
-			$config['pages_comments'] = array();
-		}
-
+		$config['pages_comments'] = isset($_POST["pages_comments"]) ? $_POST["pages_comments"] : array();
+		
 		$this->pages_comments = $config['pages_comments'];
 		$this->disqus_forum_url	= $config['disqus_forum_url'];
 
+		if( \gpFiles::SaveArray($addonPathData.'/config.php','config',$config) )
+			return message($langmessage['SAVED']);
+		message($langmessage['OOPS']);
+  	}
 
-		if( !\gpFiles::SaveArray($configFile,'config',$config) ){
-			message($langmessage['OOPS']);
-			return false;
-		}
-
-		message($langmessage['SAVED']);
-		return true;
-  }
-
-  function loadConfig(){
+  	function loadConfig(){
 		global $addonPathData;
 
-		$configFile = $addonPathData.'/config.php';
+		if(file_exists($addonPathData.'/config.php'))
+			include_once  $addonPathData.'/config.php';
 
-		if(file_exists($configFile)){
-			include_once $configFile;
-		}else{
-			$this->disqus_forum_url ='';
-			$this->pages_comments=array();
-		}
+		$this->disqus_forum_url = isset($config['disqus_forum_url']) ? $config['disqus_forum_url'] : '';
+		$this->pages_comments = isset($config['pages_comments']) ? $config['pages_comments'] : array();
+  	}
 
-
-		if (isset($config)) {
-			$this->disqus_forum_url = $config['disqus_forum_url'];
-			$this->pages_comments = $config['pages_comments'];
-		}
-  }
-
-	function get_lang_ext(){
+	function getLang(){
 		global $config;
-		$langfile = '/languages/'.$config['language'].'.php';
 
-		if(file_exists(dirname(__FILE__).$langfile)){
-			include dirname(__FILE__).$langfile;
-			}else{$langfile = '/languages/en.php';
-			include dirname(__FILE__).$langfile;
-			}
-			$this->lang_dict = $lang_dict;
+		if(file_exists(dirname(__FILE__).'/languages/'.$config['language'].'.php'))
+			include dirname(__FILE__).'/languages/'.$config['language'].'.php';
+		else
+			include dirname(__FILE__).'/languages/en.php';
+
+		return $lang_dict;
 	}
 
 }
--- Disqus.php
+++ Disqus.php
@@ -6,50 +6,29 @@ defined('is_running') or die('Not an entry point...');
 class Disqus{
 
 	function __construct(){
-			global $page,$addonPathData;
-			$configFile = $addonPathData.'/config.php';
+		global $page, $addonPathData;
 
-			if(file_exists($configFile)){
-				include_once $configFile;
-			}else {
-			$this->disqus_forum_url ='';
-			$this->pages_comments=array();
-			}
+		if(file_exists($addonPathData.'/config.php'))
+			include_once $addonPathData.'/config.php';
 
-			if (isset($config)){
-				$this->disqus_forum_url = $config['disqus_forum_url'];
-				$this->pages_comments = $config['pages_comments'];
-			}
+		if (!isset($config) || !in_array($page->gp_index, $config['pages_comments']))
+			return;
 
-			if (in_array($page->gp_index, $this->pages_comments)){
-				$this->ShowDisqus($page->gp_index,$page->title);
-				}
-		}
-
-function ShowDisqus($index,$title){
-			echo '<div id="disqus_thread"></div>';
-			echo '<script>
+		echo '<div id="disqus_thread"></div>';
+		echo '<script>
 				var disqus_config = function () {
-				this.page.title = "'.$title.'";
-				this.page.url = "'.$this->getUrl().'";
-				this.page.identifier = "'.$index.'";
+					this.page.title = "'.$page->label.'";
+					this.page.url = "'.\gp\tool::AbsoluteUrl($page->requested).'";
+					this.page.identifier = "'.$page->gp_index.'";
 				};
-
 				(function() {
-				var d = document, s = d.createElement(\'script\');
-				s.src = \'https://'.$this->disqus_forum_url.'.disqus.com/embed.js\';
-				s.setAttribute(\'data-timestamp\', +new Date());
-				(d.head || d.body).appendChild(s);
+					var d = document, s = d.createElement(\'script\');
+					s.src = \'https://'.$config['disqus_forum_url'].'.disqus.com/embed.js\';
+					s.setAttribute(\'data-timestamp\', +new Date());
+					(d.head || d.body).appendChild(s);
 				})();
-		</script>
-	<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>';
-		}
-
-	function getUrl() {
-		$url  = @( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
-		$url .= ( $_SERVER["SERVER_PORT"] != 80 ) ? ":".$_SERVER["SERVER_PORT"] : "";
-		$url .= $_SERVER["REQUEST_URI"];
-		return $url;
+			</script>';
+		echo '<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>';
 	}
 }
 ?>
