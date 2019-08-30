2801bbc9e57ac5ab7f1dc302e3831c1651af000d
Sestowner <owner@sestolab.pp.ua>
2019-08-30 18:21:19 +0300

    remove $('.comment_selector').click(function()

--- Disqus.js
+++ Disqus.js
@@ -1,17 +1,5 @@
 $(document).ready(function(){
 
-	$('.comment_selector').click(function(){
-		$(this).find('input').attr('checked', 'true');
-		var flag = $(this).find('input').val();
-		var label = $(this).find('input').attr("id");
-
-		if (flag == 'no'){
-			$(this).find('input').val(label);
-		} else {
-			$(this).find('input').val("no");
-		}
-	});
-
 	$('#edit_shortname').click(function(){
 		$('#edit_shortname>span').attr('class', 'fa fa-unlock');
 		$('#disqus_forum_url').attr('readonly', false);
